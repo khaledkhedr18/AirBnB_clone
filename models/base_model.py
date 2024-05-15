cb9ef194915871e23aec535046e8b20df2ab46ee
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-05-15 05:11:49 +0300

    ss

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,7 @@
-#!/usr/bin/python3
-"""This module defines the BaseModel class"""
+""" This module defines the BaseModel class that defines
+all common attributes/methods for other classes """
+
+
 import datetime
 import uuid
 
@@ -7,15 +9,17 @@ import uuid
 class BaseModel:
     def __init__(self, *args, **kwargs):
         """ the initialization method for the class"""
-        if not kwargs:
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.datetime.strptime(value,
+                                                       "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
-
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            self.__dict__.update(kwargs)
-        self.save()
+            self.updated_at = datetime.datetime.now()
 
     def save(self):
         """the save method for the class"""
